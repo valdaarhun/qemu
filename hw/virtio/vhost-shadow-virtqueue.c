6bd31e3087c1da30a8e23f2ae7be00dc302b20d2
Sahil Siddiq <sahilcdq@proton.me>
2024-11-03 20:27:33 +0530

    vhost-svq: ignore unsupported feats

--- hw/virtio/vhost-shadow-virtqueue.c
+++ hw/virtio/vhost-shadow-virtqueue.c
@@ -33,6 +33,9 @@ bool vhost_svq_valid_features(uint64_t features, Error **errp)
          ++b) {
         switch (b) {
         case VIRTIO_F_ANY_LAYOUT:
+        case VIRTIO_F_RING_PACKED:
+        case VIRTIO_F_RING_RESET:
+        case VIRTIO_RING_F_INDIRECT_DESC:
         case VIRTIO_RING_F_EVENT_IDX:
             continue;
 
@@ -49,6 +52,11 @@ bool vhost_svq_valid_features(uint64_t features, Error **errp)
         default:
             if (svq_features & BIT_ULL(b)) {
                 svq_features &= ~BIT_ULL(b);
+                {
+                    FILE *file = fopen("sqvlog.txt", "a");
+                    fprintf(file, "b: %ld\n", b);
+                    fclose(file);
+                }
                 ok = false;
             }
         }
